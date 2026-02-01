0a2dc1807904dbd03f43fdda1bf790eb87abbd6e
Mehmet-Yılmaz <Mhmt_ylmz_@outlook.com>
2026-02-01 03:32:02 +0300

    boss update

--- My project/Assets/Scripts/Boss.cs
+++ My project/Assets/Scripts/Boss.cs
@@ -1,16 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss : MonoBehaviour
+public class Boss : Entity
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool MoveIndex = false;
+    [SerializeField] private bool canMove = false;
+    public GameObject Target;
+    public List<GameObject> states = new List<GameObject>();
+
+    [SerializeField] private float waypointReachedDistance = 0.1f;
+
+    private int _currentStateIndex = 0;
+
+    private void OnEnable()
     {
-        
+        canMove = false;
+        StartCoroutine(EnableDelay());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator EnableDelay()
     {
-        
+        yield return new WaitForSeconds(2f);
+        canMove = true;
+    }
+
+    public override void Move()
+    {
+        if (!canMove) return;
+
+        if (MoveIndex)
+        {
+            if (Target == null) return;
+
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                Target.transform.position,
+                speed * Time.deltaTime
+            );
+            return;
+        }
+
+        if (states == null || states.Count == 0) return;
+
+        Vector3 waypointPos = states[_currentStateIndex].transform.position;
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            waypointPos,
+            speed * 1.3f * Time.deltaTime
+        );
+
+        if (Vector3.Distance(transform.position, waypointPos) <= 0.1f)
+        {
+            _currentStateIndex = (_currentStateIndex + 1) % states.Count;
+        }
+    }
+
+
+    protected void Update()
+    {
+        Move();
+    }
+
+    public override void Attack(Entity entity, float bonusattack = 0)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Death()
+    {
+        throw new System.NotImplementedException();
     }
 }
--- My project/Assets/Scripts/bossK.cs
+++ My project/Assets/Scripts/bossK.cs
@@ -1,36 +1,83 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class bossK : Boss
 {
-    public bool MoveIndex=false;
-    Transform target;
     public List<GameObject> waepons = new List<GameObject>();
-    //33 tane bomba var. ilk 22si 1 2 (yan yana) baþlayacak ve ardýndan ise 3 4 aktif olacak ve bu þekilde sýrayla her patlama belirli aralýklarla aktif olacak.
 
-    // Update is called once per frame
+    [Header("Auto Attack Settings")]
+    [SerializeField] private float autoAttackRange = 2.5f;     // dinamik yakýnlýk
+    [SerializeField, Range(0f, 1f)] private float fxBiasToTarget = 0.8f; // 0.8 => target'a yakýn
+    [SerializeField] private float fxActiveTime = 0.25f;       // anim objesini ne kadar açýk tutsun
+
+    [SerializeField]private GameObject autoAttackAnimation;
+    private Coroutine _fxRoutine;
+
+    void Awake()
+    {
+        // "auto attack animation" isimli objeyi oluþtur
+        autoAttackAnimation = new GameObject("auto attack animation");
+        autoAttackAnimation.SetActive(false);
+    }
+
     void Update()
     {
-        
-        moveFunc(MoveIndex);
         AutoAttack();
+        base.Update();
     }
 
-
-    public void moveFunc(bool Index)
-    {
-        if(MoveIndex)
-        {
-            
-        }
-        else
-        {
-            
-        }
-    }
     public void AutoAttack()
     {
+        if (!MoveIndex) return;            // takip modunda deðilse auto attack yapma (istersen kaldýr)
+        if (Target == null) return;
 
+        // target'ýn Entity componentini bul
+        Entity targetEntity = Target.GetComponent<Entity>();
+        if (targetEntity == null) return;
+
+        // cooldown sayacý
+        if (attackTimer > 0f)
+        {
+            attackTimer -= Time.deltaTime;
+            return;
+        }
+
+        // mesafe kontrolü (world)
+        float dist = Vector3.Distance(transform.position, Target.transform.position);
+        if (dist > autoAttackRange) return;
+
+        // Attack çaðýr (bossK içindeki Attack)
+        Attack(targetEntity);
+
+        // FX objesini target'a yakýn olacak þekilde iki obje arasýna koy
+        Vector3 bossPos = transform.position;
+        Vector3 targetPos = Target.transform.position;
+
+        Vector3 fxPos = Vector3.Lerp(bossPos, targetPos, fxBiasToTarget);
+        autoAttackAnimation.transform.position = fxPos;
+
+        // FX'i aç-kapat
+        if (_fxRoutine != null) StopCoroutine(_fxRoutine);
+        _fxRoutine = StartCoroutine(FxPulse());
     }
-    
+
+    private IEnumerator FxPulse()
+    {
+        autoAttackAnimation.SetActive(true);
+        yield return new WaitForSeconds(fxActiveTime);
+        autoAttackAnimation.SetActive(false);
+        _fxRoutine = null;
+    }
+
+    public void BigAttack()
+    {
+        MoveIndex = false;
+    }
+
+    public override void Attack(Entity entity, float bonusattack = 0)
+    {
+        attackTimer = attackSpeed;
+        entity.GetDamage(attackDamage + bonusattack);
+    }
 }
